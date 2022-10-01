c1671e313ac64482969d5a535df98b6f18c977c4
saint <ruslan.meloman.1986@yandex.ru>
2022-10-01 16:36:24 +0200

    ДЗ по теме "Условные операторы" .

--- src/Main.java
+++ src/Main.java
@@ -22,6 +22,21 @@ public class Main {
             default:
                 System.out.println("Для правильной работы, пожалуйста, используйте систему iOS или Android!");
         }
+        // Задание 2
+        System.out.println("Задание 2");
+        int clientDeviceYear = 2014;
 
+        if (clientOS == 0 && clientDeviceYear >= 2015) {
+            System.out.println("Ваша версия системы iOS подходит для корректной работы");
+            } else {
+            if (clientOS == 0 && clientDeviceYear < 2015)
+                System.out.println("Версия системы iOS устарела. Установите облегченную версию приложения для iOS по ссылке");
+            }
+        if (clientOS == 1 && clientDeviceYear >= 2015) {
+            System.out.println("Ваша версия системы Android подходит для корректной работы");
+            } else {
+            if (clientOS == 1 && clientDeviceYear < 2015)
+                System.out.println("Версия системы Android устарела. Установите облегченную версию приложения для Android по ссылке");
+        }
     }
 }
